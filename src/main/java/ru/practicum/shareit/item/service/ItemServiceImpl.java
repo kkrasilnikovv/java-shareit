d4e59ff73fa04646750a45a14b5cb3f39324817b
kkrasilnikovv <vania.krasilnickow@yandex.ru>
2022-11-12 12:41:17 +0300

    "fix:fixed problem"

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -73,7 +73,8 @@ public class ItemServiceImpl implements ItemService, CommentService {
 
     @Override
     public List<Item> search(String str, Long userId) {
-        return itemRepository.search(str);
+        return itemRepository.search(str).stream().sorted(Comparator.comparing(Item::getId))
+                .collect(Collectors.toList());
     }
 
     @Override
