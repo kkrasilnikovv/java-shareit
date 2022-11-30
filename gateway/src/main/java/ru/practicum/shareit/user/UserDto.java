23838fc6e19082a0de27572da0b21cac41e41f1e
kkrasilnikovv <vania.krasilnickow@yandex.ru>
2022-11-30 03:08:34 +0300

    Refactor: refactor code in gateway

--- gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.shareit.exception.validated_group.Create;
+import ru.practicum.shareit.exception.validated_group.Update;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
@@ -13,9 +17,10 @@ import javax.validation.constraints.Email;
 @AllArgsConstructor
 public class UserDto {
     private Long id;
-
+    @NotBlank(groups = {Create.class})
     private String name;
-
-    @Email
+    @Email(groups = {Create.class, Update.class})
+    @NotNull(groups = {Create.class})
     private String email;
+
 }
